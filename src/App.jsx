0c431a398b2742698e3c2a8ea7c0a401cfa14d6a
mukitur <mukit.inway@gmail.com>
2023-09-12 07:34:55 +0600

    create fake data and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Blogs from './Components/Blogs/Blogs';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   );
 }
